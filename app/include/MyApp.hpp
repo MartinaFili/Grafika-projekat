155e09c712867d430649dd8b25c1077fd8a2d893
MartinaFili <mi20198@alas.matf.bg.ac.rs>
2026-09-02 21:26:32 +0200

    Basic App setup with MyApp class

---
+++ app/include/MyApp.hpp
@@ -0,0 +1,17 @@
+//
+// Created by maki on 9/2/26.
+//
+
+#ifndef MYAPP_HPP
+#define MYAPP_HPP
+#include <engine/core/App.hpp>
+
+namespace app {
+
+class MyApp : public engine::core::App {
+    void app_setup() override;
+};
+
+}// app
+
+#endif //MYAPP_HPP
