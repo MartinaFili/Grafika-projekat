1ee0d3b1ad5bd4dfdb82080f7f0341f3b2cb24f5
MartinaFili <mi20198@alas.matf.bg.ac.rs>
2026-09-03 15:43:01 +0200

    Render rooftop model

--- app/include/MainController.hpp
+++ app/include/MainController.hpp
@@ -15,7 +15,11 @@ class MainController : public engine::core::Controller {
 
     void draw_rooftop();
 
+    void begin_draw() override;
+
     void draw() override;
+
+    void end_draw() override;
 
 public:
     std::string_view name() const override { return "app::MainController"; }
--- app/src/MainController.cpp
+++ app/src/MainController.cpp
@@ -4,12 +4,14 @@
 
 #include "../include/MainController.hpp"
 
+#include <engine/graphics/GraphicsController.hpp>
+#include <engine/graphics/OpenGL.hpp>
 #include <engine/platform/PlatformController.hpp>
 #include <engine/resources/ResourcesController.hpp>
 #include <spdlog/spdlog.h>
 
 namespace app {
-void MainController::initialize() {}
+void MainController::initialize() { engine::graphics::OpenGL::enable_depth_testing(); }
 
 bool MainController::loop() {
     auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
@@ -19,11 +21,27 @@ bool MainController::loop() {
 
 void MainController::draw_rooftop() {
     auto resources = engine::core::Controller::get<engine::resources::ResourcesController>();
-    engine::resources::Model *model = resources->model("rooftop");
+    auto graphichs = engine::core::Controller::get<engine::graphics::GraphicsController>();
+    engine::resources::Model *rooftop = resources->model("rooftop");
     engine::resources::Shader *shader = resources->shader("rooftop");
-    model->draw(shader);
+
+    shader->use();
+    shader->set_mat4("projection", graphichs->projection_matrix());
+    shader->set_mat4("view", graphichs->camera()->view_matrix());
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::translate(model, glm::vec3(0.0f, 0.0f, -3.0f));
+    model = glm::scale(model, glm::vec3(0.3f));
+    shader->set_mat4("model", model);
+    rooftop->draw(shader);
 
 }
 
+void MainController::begin_draw() { engine::graphics::OpenGL::clear_buffers(); }
+
 void MainController::draw() { draw_rooftop(); }
+
+void MainController::end_draw() {
+    auto platform = engine::core::Controller::get<engine::platform::PlatformController>();
+    platform->swap_buffers();
+}
 }// app
